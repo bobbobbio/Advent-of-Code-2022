055ea697ee704e50eb20a1d02c22d4745ad401a5
Remi Bernotavicius <remi@abort.cc>
2022-12-03 21:15:38 -0800

    Add day four

---
+++ four/src/main.rs
@@ -0,0 +1,41 @@
+#![feature(type_alias_impl_trait)]
+
+use advent::prelude::*;
+
+#[derive(HasParser, Debug, Clone)]
+#[parse(sep_by = "-")]
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn contains(&self, other: &Self) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        (self.start <= other.end && self.start >= other.start)
+            || (self.end >= other.start && self.end <= other.end)
+    }
+}
+
+#[derive(HasParser, Debug, Clone)]
+#[parse(sep_by = ",")]
+struct TwoRanges(Range, Range);
+
+#[part_one]
+fn part_one(l: List<TwoRanges, TermWith<NewLine>>) -> usize {
+    l.iter()
+        .filter(|t| t.0.contains(&t.1) || t.1.contains(&t.0))
+        .count()
+}
+
+#[part_two]
+fn part_two(l: List<TwoRanges, TermWith<NewLine>>) -> usize {
+    l.iter()
+        .filter(|t| t.0.overlaps(&t.1) || t.1.overlaps(&t.0))
+        .count()
+}
+
+harness!(part_1: 534, part_2: 841);
