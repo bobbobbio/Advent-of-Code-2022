952e77528bcc36b8fd8afa7dee6cc87476f6bb0d
Remi Bernotavicius <remi@abort.cc>
2022-12-05 21:11:30 -0800

    Add day six

---
+++ six/src/main.rs
@@ -0,0 +1,28 @@
+#![feature(type_alias_impl_trait)]
+
+use advent::prelude::*;
+use std::collections::HashSet;
+
+fn do_it(input: List<List<char, Nil>, TermWith<NewLine>>, marker_len: usize) -> usize {
+    for packet in input {
+        for i in 0..(packet.len() - marker_len) {
+            let c: HashSet<_> = packet[i..(i + marker_len)].iter().collect();
+            if c.len() == marker_len {
+                return i + marker_len;
+            }
+        }
+    }
+    panic!();
+}
+
+#[part_one]
+fn part_one(input: List<List<char, Nil>, TermWith<NewLine>>) -> usize {
+    do_it(input, 4)
+}
+
+#[part_two]
+fn part_two(input: List<List<char, Nil>, TermWith<NewLine>>) -> usize {
+    do_it(input, 14)
+}
+
+harness!(part_1: 1198, part_2: 3120);
